5ae8639d095b55cd8e10a71407a26bdd6f381f91
abortionpill-info <136628414+abortionpill-info@users.noreply.github.com>
2023-06-22 15:03:23 +0000

    Update and rename js1.json to js1.js

--- js1.js
+++ js1.js
@@ -1,5 +1,4 @@
-
-		const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const options = new chrome.Options();
 
